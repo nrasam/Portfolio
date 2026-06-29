e34382811aad3b978d80668235c22547c8e31434
Novell <nrasam@myseneca.ca>
2026-06-29 19:11:10 -0400

    -Started work on About App component

--- src/apps/AboutApp.jsx
+++ src/apps/AboutApp.jsx
@@ -1,5 +1,22 @@
+import styles from "./AboutApp.module.css";
+
 function AboutApp() {
-  return <p>Hello I'm Noel</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        {/* Profile Header */}
+        <div className={styles.profileHeader}>
+          <div className={styles.avatar}>NR</div>
+          <div className={styles.profileInfo}>
+            <h1 className={styles.name}>Noel Rasam</h1>
+            <p className={styles.title}>
+              Computer Science Student & future mad scientist
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutApp;
